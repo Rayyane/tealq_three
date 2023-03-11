7e982dc4286587978ab26a627c00f6e4b6a21576
Rayyane <akibmr9@gmail.com>
2023-03-12 00:27:56 +0600

    check if three js loads

---
+++ assets/js/custom.js
@@ -0,0 +1,64 @@
+const toggler = document.querySelector('.navbar-toggler');
+const navEl = document.querySelector('.navbar');
+var clickCount = 0;
+var width = window.innerWidth;
+toggler.addEventListener("click", function() {
+	clickCount++;
+	if (clickCount%2 != 0) {
+		navEl.classList.add('custom-bg');
+	}
+	else {
+		navEl.classList.remove('custom-bg');
+	}
+});
+console.log(width);
+/*window.addEventListener('scroll', function() {
+	if (window.innerWidth > 500 && window.scrollY >= 56) {
+		navEl.classList.add('navbar-scrolled');
+	}
+	else if (window.innerWidth > 500 && window.scrollY < 56){
+		navEl.classList.remove('navbar-scrolled');
+	}
+	else if (window.innerWidth <= 500 && window.scrollY >= 2) {
+		navEl.classList.add('navbar-scrolled');
+	}
+	else if (window.innerWidth <= 500 && window.scrollY < 2) {
+		navEl.classList.remove('navbar-scrolled');
+	}
+});*/
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    const subtitle = entry.target.querySelector('.subtitle');
+
+    if (entry.isIntersecting) {
+      subtitle.classList.add('subtitle-animation');
+	  return; // if we added the class, exit the function
+    }
+
+    // We're not intersecting, so remove the class!
+    subtitle.classList.remove('subtitle-animation');
+  });
+});
+
+observer.observe(document.querySelector('.header-text'));
+
+const aboutobserver = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    const aboutTitle = entry.target.querySelector('.about-title');
+
+    if (entry.isIntersecting) {
+      aboutTitle.classList.add('about-title-animation');
+	  return; // if we added the class, exit the function
+    }
+
+    // We're not intersecting, so remove the class!
+    aboutTitle.classList.remove('about-title-animation');
+  });
+});
+
+aboutobserver.observe(document.querySelector('.about-section-header'));
+
+VanillaTilt.init(document.querySelector(".service-img"), {
+	max: 15,
+	speed: 400
+});
---
+++ assets/js/threee.js
@@ -0,0 +1,59 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+var canvas = document.getElementById("mycanvas");
+var canvaswidth = canvas.clientWidth;
+var canvasheight = canvas.clientHeight;
+
+const renderer = new THREE.WebGLRenderer({canvas:canvas, antialias:true});
+renderer.setSize( canvaswidth, canvasheight );
+
+const light1 = new THREE.PointLight(0xffffff, 2, 100, 5);
+light1.position.set(10, 10, 10);
+scene.add(light1);
+
+const light2 = new THREE.PointLight(0xffffff, 1, 100, 5);
+light2.position.set(-10, -10, -10);
+scene.add(light2);
+
+const camera = new THREE.PerspectiveCamera( 45, canvaswidth / canvasheight, 0.1, 100 );
+camera.position.z = 9;
+
+const texture = new THREE.TextureLoader().load('./assets/images/moon_texture_2.jpg');
+
+const geometry = new THREE.SphereGeometry(3, 64, 64 );
+const material = new THREE.MeshStandardMaterial( {
+ color: 0x008080,
+ map: texture
+} );
+const sphere = new THREE.Mesh( geometry, material );
+scene.add( sphere );
+
+renderer.render( scene, camera );
+renderer.setClearColor("#ffffff", 0.0);
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 4;
+
+window.addEventListener("resize", () => {
+	const canvas = document.getElementById("mycanvas");
+	const canvaswidth = canvas.clientWidth;
+    const canvasheight = canvas.clientHeight;
+
+    camera.aspect = canvaswidth / canvasheight;
+    camera.updateProjectionMatrix();
+    console.log(canvaswidth, canvasheight, camera.aspect)
+	renderer.setSize( canvaswidth, canvasheight );
+});
+
+const loop = () => {
+	controls.update();
+	renderer.render(scene, camera);
+	window.requestAnimationFrame(loop);
+}
+loop();
